bd05fb6b678478e18933b5ba055dc7f8369d2ec9
tatoMa <Jason.ma1904@gmail.com>
2019-08-22 21:05:02 +1000

    add simple search function of cities

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
+    {{searchText}}
+    <input v-model="searchText">
+    <button v-on:click="searchCity">SEARCH</button>
     <div v-if="search">
       <div v-for="city in search" :key="city.woeid">
-        {{city.title}}
+        <div v-on:click="selectCity(city.woeid)">
+          {{city.title}}
+        </div>
       </div>
     </div>
     <br>
     <!-- <Weather/> -->
     <div v-if="result">
     {{result.title}}
-    {{new Date(result.time).getMonth()+1}} - 
+    {{new Date(result.time).getMonth()+1}} -
     {{new Date(result.time).getDate()}}
     <hr>
     <!-- {{result.consolidated_weather}} -->
@@ -19,7 +24,7 @@
         {{daily.weather_state_name}}
         {{Math.round(daily.the_temp)}}
         <br>
-        wind: 
+        wind:
         <br>
         {{daily.wind_speed.toFixed(2)}} mph
         {{daily.wind_direction.toFixed(0)}} degrees
@@ -44,22 +49,31 @@ export default {
     return {
       result: {},
       search: {},
-      searchText: 'new',
+      searchText: '',
+      resultCity: '',
     };
   },
   async mounted() {
-    const result = await fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/44418/')
-      .then(data => data.json()
-        .then((res) => {
-          console.log(res);
-          this.result = res;
-        }));
-    await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${this.searchText}`)
-      .then(data => data.json()
-        .then((res) => {
-          console.log(res);
-          this.search = res;
-        }));
+    this.selectCity('Melbourne');
+    this.searchCity();
+  },
+  methods: {
+    async searchCity() {
+      await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${this.searchText}`)
+        .then(data => data.json()
+          .then((res) => {
+            console.log(res);
+            this.search = res;
+          }));
+    },
+    async selectCity(selectCity) {
+      const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${selectCity}/`)
+        .then(data => data.json()
+          .then((res) => {
+            console.log(res);
+            this.result = res;
+          }));
+    },
   },
 };
 </script>
